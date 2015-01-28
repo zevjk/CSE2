6c5a8ee88e07ab64bb90ab83ac4803c42a087934
Zev Koffsky <zkoffsky@gmail.com>
2015-01-28 03:44:10 +0000

    hw01

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+////////////////////////////////////////////////////////////////////////////
+//lab 1
+//Zev Koffsky
+//1/23/15
+// define a class
+public class HelloWorld{
+    
+// add main method
+    public static void main(String[] args) {
+        
+// print the statement with box around it
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("#                     #");
+        System.out.println("# Hello World 2015 !! #");
+        System.out.println("#                     #");
+        System.out.println("#######################");
+        System.out.println("#######################");
+        System.out.println("test");
+        
+        //test/testtest
+    }
+}
