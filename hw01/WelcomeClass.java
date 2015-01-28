74f54140cf4048062e77149625a06e7de1d8984b
Zev Koffsky <zkoffsky@gmail.com>
2015-01-28 03:45:29 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////
+//lab 1
+//Zev Koffsky
+//1/23/15
+// define a class
+public class Intro{
+    
+// add main method
+    public static void main(String[] args) {
+        
+// print the statement with box around it
+        System.out.println("    -----------     ");
+        System.out.println("    | WELCOME |     ");
+        System.out.println("    -----------     ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("<-Z--J--K--2--1--8->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v  ");
+        System.out.println("I am an Eagle Scout and I've been a rower for 5 and a half years.");
+        
+        //test/testtest
+    }
+}
